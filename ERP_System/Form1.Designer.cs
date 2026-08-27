3ae6b73a0c095d6653b8e6ec868279bae7f53a05
ChedadSoumia <chedadsoumia4@gmail.com>
2026-08-27 15:48:01 -0700

    Add Category Classes DataAccess And Business

--- ERP_System/Form1.Designer.cs
+++ ERP_System/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(63, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(675, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(367, 117);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(588, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ ERP_System_DataAccess/clsCategoryData.cs
@@ -0,0 +1,239 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_System_DataAccess
+{
+    public static class clsCategoryData
+    {
+        public static bool GetCategoryByID(int CategoryID, ref string CategoryName, ref int? CategoryParentID)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_GetCategoryByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Category_id", CategoryID);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Process results
+                            while (reader.Read())
+                            {
+                                isFound = true;
+                                CategoryName = (string)reader["Category_name"];
+
+
+                                if (reader["Category_parent"] != DBNull.Value)
+                                {
+                                    CategoryParentID = (int)reader["Category_parent"];
+                                }
+                                else
+                                {
+                                    CategoryParentID = null;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return isFound;
+        }
+
+        public static bool GetCategoryByName(ref int CategoryID, string CategoryName, ref int? CategoryParentID)
+        {
+            bool isFound = false;
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_GetCategoryByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Category_name", CategoryName);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Process results
+                            while (reader.Read())
+                            {
+                                isFound = true;
+                                CategoryID = (int)reader["Category_id"];
+
+
+                                if (reader["Category_parent"] != DBNull.Value)
+                                {
+                                    CategoryParentID = (int)reader["Category_parent"];
+                                }
+                                else
+                                {
+                                    CategoryParentID = null;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return isFound;
+        }
+
+        public static DataTable GetAllCategories()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_GetAllCategories", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            dt.Load(reader);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+            }
+
+            return dt;
+        }
+
+        public static int AddNewCategories(string CategoryName, int? CategoryParent)
+        {
+            int CatrgoryID = -1;
+
+            try
+            {
+                using (SqlConnection connection =
+                       new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_AddNewCategory", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Category_name", CategoryName);
+
+                        if (CategoryParent.HasValue)
+                            command.Parameters.AddWithValue("@Category_Parent", CategoryParent);
+                        else
+                            command.Parameters.AddWithValue("@Category_Parent", System.DBNull.Value);
+
+                        SqlParameter outputIdParam = new SqlParameter("@Category_id", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        CatrgoryID = (int)command.Parameters["@Category_id"].Value;
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+            }
+
+
+
+
+            return CatrgoryID;
+        }
+
+        public static bool UpdateCategory(int CategoryID, string CategoryName, int? CategoryParent) {
+            int rowsAffected = 0;
+
+            try
+            {
+                using(SqlConnection connection =new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_UpdateCategory", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@@Category_id", CategoryID);
+                        command.Parameters.AddWithValue("@Category_name", CategoryName);
+
+                        if (CategoryParent.HasValue)
+                            command.Parameters.AddWithValue("@Category_Parent", CategoryParent);
+                        else
+                            command.Parameters.AddWithValue("@Category_Parent", System.DBNull.Value);
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (Exception ex) { return false; }
+
+            return (rowsAffected > 0);
+        }
+
+        public static bool IsCategoryExist(string CategoryName)
+        {
+            int result ;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("spu_CheckCategoryExists", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Category_name", CategoryName);
+
+                        result = (int)command.ExecuteScalar();
+
+                        return (result == 1);
+                    }
+
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+}
+
